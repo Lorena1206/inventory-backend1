133fd82696358e706eccbd58cabac8b8001948cd
lorenaH <hlorena27@gmail.com>
2023-10-27 17:22:41 -0500

    buscar categoria por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,27 @@ public class CategoryRestController {
 	
 	@Autowired
 	private ICategoryService service;
+	
+	/**
+	 * get all the categories
+	 * @return
+	 */
+	
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategory(){
 		ResponseEntity<CategoryResponseRest> response = service.Search();
+		return response;
+		
+	}
+	
+	/**
+	 * get categories by id
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoryById(@PathVariable Long id){
+		ResponseEntity<CategoryResponseRest> response = service.SearchById(id);
 		return response;
 		
 	}
--- src/main/java/com/company/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/services/ICategoryService.java
@@ -7,5 +7,6 @@ import com.company.inventory.response.CategoryResponseRest;
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> Search();
+	public ResponseEntity<CategoryResponseRest> SearchById(Long id);
 
 }
